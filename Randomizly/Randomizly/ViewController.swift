49c41f972c6ba19a9f3e00a01134ab699fb9d36b
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-04-19 14:33:40 -0500

    add label

--- Randomizly/Randomizly/ViewController.swift
+++ Randomizly/Randomizly/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var randomNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.randomNumberLabel.text = "10"
     }
 
 }
