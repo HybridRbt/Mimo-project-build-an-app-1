13c13fd16fe5b4c0283f7ddf056f0e5e89246a9b
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-04-21 12:31:25 -0500

    remove from first view controller

--- Randomizly/Randomizly/ViewController.swift
+++ Randomizly/Randomizly/ViewController.swift
@@ -10,32 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var randomNumberLabel: UILabel!
-    @IBAction func newRandomNumber(_ sender: Any) {
-        self.number = Int(arc4random_uniform(6)) + 1
-        self.randomNumberLabel.text = "\(self.number)"
-        self.setBackgroundColorRandomly()
-    }
     
-    var number = 10
-    var colorArray = [UIColor.red, UIColor.green, UIColor.blue]
-    
-    func getRandomColorIndex() -> Int {
-        return Int(arc4random_uniform(3)) // return 0, 1, 2 randomly
-    }
-    
-    func setBackgroundColorRandomly() {
-        let colorIndex = getRandomColorIndex()
-        self.view.backgroundColor = colorArray[colorIndex]
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //self.view.backgroundColor = UIColor.red
-        self.newRandomNumber(self)
-        self.setBackgroundColorRandomly()
-    }
-
 }
 
