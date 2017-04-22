0e40ee5861caa72f2d644f398ed18925d91f73e3
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-04-22 13:58:59 -0500

    print out the inputs

--- Randomizly/Randomizly/GameViewController.swift
+++ Randomizly/Randomizly/GameViewController.swift
@@ -12,6 +12,12 @@ class GameViewController: UIViewController {
 
     @IBOutlet weak var inputField: UITextField!
     @IBAction func guess(_ sender: Any) {
+        if let input = self.inputField.text, input != "" {
+            print("Input: \(input)")
+        } else {
+            print("No input")
+        }
+    
     }
     
 }
