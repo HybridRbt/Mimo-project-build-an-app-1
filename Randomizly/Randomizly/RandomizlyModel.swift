97a5fae39b7530dd7d0e6c79c055bd2a8ac1757f
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-04-22 14:15:06 -0500

    add Randomizly Model

---
+++ Randomizly/Randomizly/RandomizlyModel.swift
@@ -0,0 +1,25 @@
+//
+//  RandomizlyModel.swift
+//  Randomizly
+//
+//  Created by Jianfeng Yang on 4/22/17.
+//  Copyright © 2017 Jianfeng Yang. All rights reserved.
+//
+
+import Foundation
+
+class RandomizlyModel {
+    var number = 0
+    
+    init() {
+        self.newNumber()
+    }
+    
+    func newNumber() {
+        self.number = Int(arc4random_uniform(6)) + 1
+    }
+    
+    func guess(_ guess: Int) -> Bool {
+        return self.number == guess 
+    }
+}
