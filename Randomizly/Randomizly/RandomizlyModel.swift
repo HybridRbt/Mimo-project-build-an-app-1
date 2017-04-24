bfb08787a4017742c628a6942c956263dc605ef1
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-04-23 23:11:27 -0500

    add enum to give hint on the guesses

--- Randomizly/Randomizly/RandomizlyModel.swift
+++ Randomizly/Randomizly/RandomizlyModel.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+enum GuessAnswer {
+    case correct
+    case tooLow
+    case tooHigh
+}
+
 class RandomizlyModel {
     private var number = 0
     private(set) var tries = 0
@@ -20,8 +26,14 @@ class RandomizlyModel {
         self.number = Int(arc4random_uniform(6)) + 1
     }
     
-    func guess(_ guess: Int) -> Bool {
+    func guess(_ guess: Int) -> GuessAnswer {
         self.tries += 1
-        return self.number == guess
+        if number == guess {
+            return .correct
+        } else if number > guess {
+            return .tooLow
+        } else {
+            return .tooHigh
+        }
     }
 }
