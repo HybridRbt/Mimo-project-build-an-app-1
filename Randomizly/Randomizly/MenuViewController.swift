7e88ca4f5cd85b707cfd23c85b91e0eb08a82bd6
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-04-24 21:29:12 -0500

    add a timer to change the background color
    of the menu view.

--- Randomizly/Randomizly/MenuViewController.swift
+++ Randomizly/Randomizly/MenuViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    var backgroundColorArray = [UIColor.cyan, UIColor.purple, UIColor.red]
+    var backgroundColorIndex = 0
+    var timer = Timer()
     
+    func setTimer() {
+        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(self.updateBackground)), userInfo: nil, repeats: true)
+    }
+    
+    func updateBackground() {
+        self.view.backgroundColor = self.backgroundColorArray[backgroundColorIndex]
+        backgroundColorIndex += 1
+        if backgroundColorIndex > 2 {
+            backgroundColorIndex = 0
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTimer()
+    }
 }
 
